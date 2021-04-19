6a2d4d731c592e0273bc562248ff00d377bc4c93
jo FERREIRA <job_junior_costa@hotmail.com>
2021-04-19 10:39:48 -0400

    Adicionar arquivos de projeto.

---
+++ MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace App01
+{
+    [Activity(Label = "Meu Primeiro App", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumero;
+        int number;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            // vai os códigos
+            txtNumero = FindViewById<TextView>(Resource.Id.txtNumero);
+            FindViewById<Button>(Resource.Id.btnIncrementar).Click += (o, e) =>
+            txtNumero.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrementar).Click += (o, e) =>
+             txtNumero.Text = (--number).ToString();
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
